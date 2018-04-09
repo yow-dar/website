1dec84059c4a3e8343dfd2f3a23b7da866783de1
yow-dar <wz991618@gmail.com>
2018-04-09 21:04:20 +0800

    change animation timing

--- website.js
+++ website.js
@@ -2,12 +2,12 @@ $(document).ready(function(){
   $("#background1").delay(2000).fadeIn(2000);
   $(".content").delay(4000).fadeIn(2000);
   $("#background1").delay(18000).fadeOut(1000);
-  $(".content").delay(18000).fadeOut(1000);
+  //$(".content").delay(18000).fadeOut(1000);
   $(".background2").delay(19000).show();
   $(".content2").delay(19000).fadeIn(2000);
-  $(".background2").delay(21500).fadeOut(3000);
-  $(".content2").delay(25000).fadeOut(3000);
-  $(".background3").delay(28000).show();
+  $(".background2").delay(31500).fadeOut(3000);
+  //$(".content2").delay(35000).fadeOut(3000);
+  $(".background3").delay(38000).show();
   var boatdown = document.getElementById("boat_down");
   var boatup = document.getElementById("boat_up");
   $(boatdown).hover(function(){
