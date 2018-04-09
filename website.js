003600cf3d4e377922b405509883f80934e62593
yow-dar <wz991618@gmail.com>
2018-04-09 20:21:35 +0800

    homework3

--- website.js
+++ website.js
@@ -1,9 +1,21 @@
 $(document).ready(function(){
-  $(".background1");
-  $(".background2").hide();
-  $(".boat").hide();
+  $("#background1").delay(2000).fadeIn(2000);
+  $("#content").delay(0).fadeIn(2000);
+  $("#background1").delay(12000).fadeOut(1000);
+  $(".background2").show();
+  $(".content2").delay(1000).fadeIn(2000);
+  $(".background2").delay(500).fadeOut(3000);
+  $(".content2").delay(500).fadeOut(3000);
+  $(".background3").show();
+  var boatdown = document.getElementById("boat_down");
+  var boatup = document.getElementById("boat_up");
+  $(boatdown).hover(function(){
+    $(boatdown).css('opacity',0.5);
+    $(boatup).css('opacity',1);
+    $(boatup).css('right','+5%');
+  });
   //left mouse onclick
   $("Left_House").click(function(){
-    
+    $(boatup).animate({opacity: 0.5, left: "100px"},1000);  
   })
 });
