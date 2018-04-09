bef35f8d7a1b523fdf22ca9ff721c96a640c971b
yow-dar <wz991618@gmail.com>
2018-04-09 20:58:14 +0800

    fix js

--- website.js
+++ website.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
   $(".content").delay(4000).fadeIn(2000);
   $("#background1").delay(18000).fadeOut(1000);
   $(".content").delay(18000).fadeOut(1000);
-  $(".background2")delay(19000).show();
+  $(".background2").delay(19000).show();
   $(".content2").delay(19000).fadeIn(2000);
   $(".background2").delay(21500).fadeOut(3000);
   $(".content2").delay(25000).fadeOut(3000);
